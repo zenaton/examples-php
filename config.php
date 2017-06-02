631ebf7b8c46c342f00db4f48ea5428ec7643a56
Louis Cibot <louiscibot@gmail.com>
2017-06-02 12:19:09 +0200

    remove old unused files

--- config.php
+++ config.php
@@ -3,18 +3,3 @@
 // $app_id = '';
 // $app_env = 'dev';
 // $api_token = '';
-
-// // Gilles
-// $app_id = 'QSXZBVWFKN';
-// $app_env = 'gilles';
-// $api_token = 'RReVsCeBF6EdbUvBDI4O7L9eSyD5Xoi08rdvWmFDPIcjFY0aKfLWVcs972DR';
-
-// Louis prod
-// $app_id = 'JZNHGKXEBY';
-// $app_env = 'production';
-// $api_token = '2R8hjkeP7wQ0FizAXdgcb33VVWoW9JH4Pbs8hDV3U3a5fmIbPxeA0fDeySTP';
-
-// Louis dev
-$app_id = 'XPIAGHJZBE';
-$app_env = 'production';
-$api_token = '5j9QXUTiwSDA0QFh3WUw5wNG5FpW6YV4aV3E8s13LmzgvARtTCPmPwwkwV1l';
