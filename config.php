65c9945fb113a0c7cd0af5fa4d65145c7b2ccd81
Louis Cibot <louiscibot@gmail.com>
2017-07-07 11:25:55 +0200

    dev

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$app_id = 'FPBYRXSICA';
-$app_env = 'dev';
-$api_token = 'dDH8sglAiL9EGTDrWMWRmH4cEPckYdTqgbEya2fpZ72MSQ5wxXhc8baUMAuI';
+$app_id = 'CJXLYEBOQW';
+$app_env = 'staging';
+$api_token = 'oVJIKOViBYNPPJyc6ZvLE4dMSC26kYgjIb40UGsofaCg15ZsUNCtt57his2f';
